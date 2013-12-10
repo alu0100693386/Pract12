40be5a8605cdda22b311c5d5128a14b532a7fffe
Cristian Luis <alu0100693386@ull.edu.es>
2013-12-10 12:34:50 +0000

    Inclusion de primeras funciones y primeras pruebas del codigo

--- lib/matriz/InterfazDSL.rb
+++ lib/matriz/InterfazDSL.rb
@@ -1,28 +1,54 @@
 class MatrixDSL
-  attr_accessor :option, :operand
+	attr_accessor :option, :operand
 
-  def initialize(operacion, &block)
-    self.option = []
-    self.operand = []
+  		def initialize(operacion, &block)
+			@operacion = operacion
+    	
+			self.option = []
+    			self.operad = []
+    			@tipo = 'densa'
+  			@mostrar = 'console'  
 
-    if block_given?  
-      if block.arity == 1
-        yield self
-      else
-        instance_eval &block 
-      end
-    end
-  end
+    			#if block_given?  
+      			#	if block.arity == 1
+        		#		yield self
+      			#	else
+        				instance_eval &block 
+      			#	end
+    			#end
+			
+			puts self
+  		end
   
-  def to_s
-    
-  end
+  	def to_s
+ 		salida = "\n #{@operacion} \n"
+		
+		case operacion
+      			when "Suma" then salida << " #{(operador[0]+operandor[1]).to_s}\n"
+      			when "Resta" then salida << " #{(operador[0]+operandor[1]).to_s}\n"
+      			when "Producto" then salida << " #{(operador[0]+operandor[1]).to_s}\n"        
+    		end 
+		salida  
+  	end
 
-  def option(opcion)
-  end
+  	def option(opcion)
+		case opcion
+      			when 'densa','dispersa' then @tipo = opcion
+      			when 'console','file' then @mostrar = opcion
+    		end
+  	end
 
-  def operand(array) 
-  end
+  	def operand(array) 
+		vector = []
+    		array.each { |x| x.each { |y| vector << y } }
+ 
+    		case @tipo
+      			when "densa" then operandor << Matriz.new(array.size,array[0].size,aux)
+      			when "dispersa" then operandor << Matriz_dispersa.new(array.size,array[0].size,aux,"COO")
+    		end   			
+				
+		
+  	end
 end
 
 
